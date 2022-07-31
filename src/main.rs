df92e17ee2051470f974a3b90aa66d5fa8332ad2
PacifiK2460 <chago1929@hotmail.com>
2022-07-30 20:57:35 -0500

    small implements

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,73 @@
 mod window;
-use crossterm::event::{read, Event};
+
+use std::io::{Write, stdout};
+
+use crossterm::{*, terminal::*, cursor::*, style::{Stylize, Color}};
+
 
 fn main(){
-    let _main = window::menu::menu::new(
+
+    match execute!(stdout(), EnterAlternateScreen) {
+        Ok(_) => {},
+        Err(e) => {
+            panic!("No se pudo iniciar el programa: {}", e);
+        }
+    }
+
+    let main = window::menu::menu::new(
+        "Menu Principal".to_string(),
         vec![
             window::menu::Opcion::new(
-                "Titulo".to_string(),
-                "Descripcion".to_string(),
-                || {
-                    println!("Accion");
-                }
+                "Consulta de almacén".to_string(),
+                "Enlista los productos disponibles".to_string(),
+                todo!(),
             ),
+
             window::menu::Opcion::new(
-                "Titulo".to_string(),
-                "Descripcion".to_string(),
-                || {
-                    println!("Accion");
-                }
+                "Actualizar almacén".to_string(),
+                "Sumar / Modificar inventario, agregar productos".to_string(),
+                todo!(),
             ),
+
+            window::menu::Opcion::new(
+                "Registrar Pedido".to_string(),
+                "Registrar información del pedido y del cliente".to_string(),
+                todo!(),
+            ),
+
+            window::menu::Opcion::new(
+                "Consultar Pedido".to_string(),
+                "Enlista los pedidos por Activos, Entregados, Cancelados e ID".to_string(),
+                todo!(),
+            ),
+
+            window::menu::Opcion::new(
+                "Registrar entrega".to_string(),
+                "Registra la entrega de un pedido y actualiza el inventario".to_string(),
+                todo!(),
+            ),
+
+            window::menu::Opcion::new(
+                "Modificar estado de pedido".to_string(),
+                "Modifica el estado del pedido del cliente".to_string(),
+                todo!(),
+            ),
+
+            window::menu::Opcion::new(
+                "Salir del sistema".to_string(),
+                "Cierra la sesión actualCierra la sesión actual".to_string(),
+                todo!(),
+            )
         ]
     );
 
+    main.draw();
 
-    loop {
-        // `read()` blocks until an `Event` is available
-        match read().unwrap() {
-            Event::Key(event) => println!("{:?}", event),
-            Event::Mouse(event) => println!("{:?}", event),
-            Event::Resize(width, height) => println!("New size {}x{}", width, height),
+    match execute!(stdout(), LeaveAlternateScreen) {
+        Ok(_) => {},
+        Err(e) => {
+            panic!("No se pudo cerrar el programa: {}", e);
         }
-    }
+    };
+
 }
-
-
--- src/window/menu/mod.rs
+++ src/window/menu/mod.rs
@@ -1,3 +1,6 @@
+use crossterm::{*, terminal::*, cursor::*, style::{Stylize, Color}};
+
+
 pub struct Opcion {
     titulo: String,
     descripcion: String,
@@ -5,17 +8,9 @@ pub struct Opcion {
 }
 
 pub struct menu {
+    titulo: String,
     opciones: Vec<Opcion>,
     selected: i32,
-}
-
-impl menu {
-    pub fn new(opciones: Vec<Opcion>) -> menu {
-        menu {
-            opciones,
-            selected: 0,
-        }
-    }
 }
 
 impl Opcion {
@@ -24,6 +19,44 @@ impl Opcion {
             titulo,
             descripcion,
             accion,
+        }
+    }
+}
+
+impl menu {
+    pub fn new(titulo: String, opciones: Vec<Opcion>) -> menu {
+        menu {
+            titulo,
+            opciones,
+            selected: 0,
+        }
+    }
+
+    pub fn draw(&self) {
+        loop {
+            // Limpia la pantalla
+            crossterm::terminal::Clear(crossterm::terminal::ClearType::All);
+            // Imprimimos el titulo
+            {
+                crossterm::cursor::MoveTo(4, 2);
+                print!("{}", 
+                    self.titulo.clone()
+                        .with(Color::Rgb{r: 255, g: 255,b: 255})
+                        .on(Color::Rgb{r: 16,g:  158,b: 94})
+                );
+            }
+
+            // Imprimimos las opciones
+            {
+
+            }
+
+            // Imprimirmos la ayuda
+
+            // Ejecutamos la funcion seleccionada
+            {
+
+            }
         }
     }
 }
