a84380fd3a41041c37d4ec989d91554af1808f17
PacifiK2460 <chago1929@hotmail.com>
2022-07-31 15:19:14 -0500

    menu input handler

--- src/window/menu/mod.rs
+++ src/window/menu/mod.rs
@@ -1,5 +1,5 @@
-use crossterm::{*, terminal::*, cursor::*, style::{Stylize, Color}};
-
+use crossterm::{*, terminal::*, cursor::*, style::{Stylize, Color}, event::{read, Event, KeyEvent, KeyCode}};
+use crossterm::event::Event::Key;
 
 pub struct Opcion {
     titulo: String,
@@ -46,17 +46,60 @@ impl menu {
                 );
             }
 
-            // Imprimimos las opciones
-            {
-
-            }
-
             // Imprimirmos la ayuda
+            {
+                todo!();                
+            }
+            
+            // Imprimimos las opciones
+            self.focus();
 
             // Ejecutamos la funcion seleccionada
             {
 
             }
         }
+    }
+
+    fn focus(&mut self){
+        loop {
+            // Imprimimos las opciones
+            self.display();
+            // Capturamos el evento
+            match read() {
+                Ok(event) => {
+                    match event {
+                        Key(key) => {
+                            match key.code {
+                                KeyCode::Up => {
+                                    if self.selected > 0 {
+                                        self.selected -= 1;
+                                    }
+                                },
+                                KeyCode::Down => {
+                                    if self.selected < self.opciones.len() as i32 - 1 {
+                                        self.selected += 1;
+                                    }
+                                },
+                                KeyCode::Enter => {
+                                    (self.opciones[self.selected as usize].accion)();
+                                }
+                                KeyCode::Left => todo!(),
+                                KeyCode::Null => panic!("No se puede usar la tecla NULL"),
+                                _ => {},
+                            }
+                        },
+                        _ => {}
+                    }
+                },
+                Err(e) => {
+                    panic!("Error: {}", e);
+                }
+            }
+        }
+    }
+
+    fn display(&self){
+        todo!();
     }
 }
